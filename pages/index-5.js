fc622a65392361ae41eb92b92569caa63949c509
algosbiz <contact@algoseabiz.com>
2026-03-16 16:34:26 +0800

    update

--- pages/index-5.js
+++ pages/index-5.js
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import Banner from "../components/sections/Home5/Banner";
 import About from "../components/sections/Home3/About";
-import Video from "../components/sections/Home5/Video";
 import Gallery from "../components/sections/Home5/Gallery";
 import Services from "../components/sections/Home5/Services";
 import Marquee from "../components/sections/Home5/Marquee";
@@ -13,22 +12,24 @@ import Testimonial from "../components/sections/Home3/Testimonial";
 import News from "../components/sections/Home5/News";
 import PackageSection from "@/components/sections/Home1/Package";
 import Feature from "../components/sections/Home1/Feature";
-
+import Video from "../components/sections/Home1/Video";
 export default function Home5() {
     return (
         <>
             <Layout HeaderStyle="five" FooterStyle="three">
                 <Banner />
+                <Step />
                 <About />
                 <Feature />
-                <Step />
-                <Gallery />
+
+                {/* <Gallery /> */}
 
                 {/* <Video /> */}
                 {/* <Services /> */}
                 <Marquee />
                 <Testimonial />
                 <PackageSection />
+                <Video />
                 <Pricing />
                 {/* <Contact /> */}
                 {/* <News /> */}
